c5ec9cfddd78d6564cb0219270e271205446702d
sounak1407 <51594435+sounak1407@users.noreply.github.com>
2020-10-14 02:07:53 +0530

    Add files via upload

---
+++ sounak_1/script.js
@@ -0,0 +1,67 @@
+// function add(a,b){
+//     var sum = a+b;
+//     return sum;
+// }
+
+// console.log(add(2,3));
+
+// var add = function(a,b){
+//     return a+b;
+//     // or
+//     // return console.log(a+b)
+// }
+
+// add(2,3);
+
+// // calling function itself
+// var plus = function(a,b){
+
+//     return (
+//         console.log(this);
+//         console.log(a+b);)
+// }();
+// // passing the variables/
+// // (2,3)
+
+// plus(3,5)
+
+// var arr[]= {'sounak','das'};
+
+// var calculator = {
+//     name : 'My calc',
+//     plus : function(a,b){
+//         return a+b
+//     }
+// }
+
+// calculator.plus(2,3);
+
+
+// var name{
+//     name: 'sounak das';
+// }
+
+// console.log(name.name);
+
+// var calc = new calculator();
+// calc.a = 10;
+// calc.b = 20;
+
+
+var calc1 = function(a,b){
+    console.log(a+b);
+}
+
+
+var calc2 = function(a,b){
+    console.log(a*b);
+}
+
+
+
+cal1 = new calc1(2,3);
+cal2 = new calc2(5,2);
+
+console.log(cal1);
+console.log(cal2);
+
